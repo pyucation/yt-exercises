e03b3fa92b8d4c934d59921c3c4d5891c0c2fdfa
Soxxes <hscschwimmermarc@t-online.de>
2023-02-21 06:31:51 +0100

    added: more functions - dec to any

---
+++ 2_Intermediate/0_more-functions/1_decimal_to_any.py
@@ -0,0 +1,14 @@
+"""
+Topic: Functions
+Difficulty: 3/5
+Requires: functions, collections (lists), indexing, loops
+
+Task:
+Write a function that converts a decimal number to a number of any other
+number system with a base smaller than 10.
+
+Hint: Use a while loop.
+
+Additional Task: What about hex numbers? Provide another function that uses your
+first function and converts the result to hexadecimal.
+"""
