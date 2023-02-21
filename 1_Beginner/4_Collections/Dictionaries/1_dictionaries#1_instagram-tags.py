c13c95496b71db599cf3941a5b5a962b2744d716
Soxxes <hscschwimmermarc@t-online.de>
2023-02-21 07:57:11 +0100

    added: dict ex #1

---
+++ 1_Beginner/4_Collections/Dictionaries/1_dictionaries#1_instagram-tags.py
@@ -0,0 +1,22 @@
+"""
+Topic: Dictionaries and sorted function
+Difficulty: 2/5
+Requires: lists, tuples, indexing
+
+Task:
+You are given a table of instgram tags with a number expressing the popularity
+of its tag. Create a dictionary and identify the top 3 tags.
+Then, change the popularity of the tag "AI" to 1.5, because AI is cool.
+Finally, add another tag to the dictionary that is 5 times more popular than the
+second lowest tag.
+
+tag     |   popularity
+----------------------
+gym     |   1.8 M
+magic   |   0.2 M
+AI      |   0.9 M
+joebiden|   0.7 M
+football|   2.4 M
+tesla   |   1.3 M
+"""
+
